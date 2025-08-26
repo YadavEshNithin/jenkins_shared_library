3ec302b1ad55ec2454f2f8b39b530fb9c1ff8b67
YadavEshnithin <eshnithinyadav@gmail.com>
2025-08-26 12:28:08 +0530

    update

--- vars/nodejsEksPipeline.groovy
+++ vars/nodejsEksPipeline.groovy
@@ -102,11 +102,19 @@ def call (Map configMap){
             stage('Docker build') {
                 steps {
                     script{
-                        withAWS(credentials: 'aws-credds', region: 'us-east-1') {
+                        // withAWS(credentials: 'aws-credds', region: 'us-east-1') {
+                        //     sh """
+                        //         aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
+                        //         docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
+                        //         docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
+                        //     """
+                        // }
+                        withAWS(credentials: 'aws-creds', region: 'us-east-1') {
                             sh """
                                 aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
                                 docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
                                 docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
+                                aws ecr wait image-scan-complete --repository-name ${PROJECT}/${COMPONENT} --image-id imageTag=${appVersion} --region ${REGION}
                             """
                         }
                     }
