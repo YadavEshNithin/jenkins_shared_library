b61a4256936de4ee9528ce2791eeee1c1af1f8cd
YadavEshnithin <eshnithinyadav@gmail.com>
2025-08-26 12:15:08 +0530

    update

--- vars/nodejsEksPipeline.groovy
+++ vars/nodejsEksPipeline.groovy
@@ -173,7 +173,7 @@ def call (Map configMap){
                     //         echo "✅ No HIGH/CRITICAL vulnerabilities found."
                     //     }
                     // }
-                    withAWS(credentials: 'aws-creds', region: 'us-east-1') {
+                    withAWS(credentials: 'aws-credds', region: 'us-east-1') {
                             sh """
                                 aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
                                 docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
