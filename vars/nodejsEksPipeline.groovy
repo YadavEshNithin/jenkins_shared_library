be63e419176a3ce927a3ca231785e6eca78ff6e6
YadavEshnithin <eshnithinyadav@gmail.com>
2025-08-26 12:29:29 +0530

    update

--- vars/nodejsEksPipeline.groovy
+++ vars/nodejsEksPipeline.groovy
@@ -109,7 +109,7 @@ def call (Map configMap){
                         //         docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
                         //     """
                         // }
-                        withAWS(credentials: 'aws-creds', region: 'us-east-1') {
+                        withAWS(credentials: 'aws-credds', region: 'us-east-1') {
                             sh """
                                 aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
                                 docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
